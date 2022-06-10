6cef69704a3e37d378f8500b33c21502a5534ba8
imtiazhabib17 <imtiazhabib17@gmail.com>
2022-06-10 10:37:14 +0600

    first commit

---
+++ CV/CV/Controllers/ResumeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class ResumeController : Controller
+    {
+        // GET: Resume
+        public ActionResult Resume()
+        {
+            return View();
+        }
+    }
+}
